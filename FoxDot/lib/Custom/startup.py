b46047c55b0947dc5a9d112b645bf553f9e6b9e9
zbdm <lagouttedo@gmail.com>
2020-04-29 16:47:52 +0200

    python 3.8 warning, mod print_sample, add print_loops

--- FoxDot/lib/Custom/startup.py
+++ FoxDot/lib/Custom/startup.py
@@ -8,10 +8,12 @@ import time
 from .Settings import FOXDOT_ROOT
 from .Buffers import alpha, nonalpha
 # copy / paste code
-import pyperclip as clip
-# ASCII GENERATOR
-from pyfiglet import figlet_format
-
+try:
+	import pyperclip as clip
+	# ASCII GENERATOR
+	from pyfiglet import figlet_format
+except:
+	print("Please install pyperclip & pyfiglet")
 
 #########################
 ### SERVER CONFIG     ###
@@ -46,17 +48,20 @@ bpm_intro = int(server_data["bpm_intro"])
 scale_intro = str(server_data["scale_intro"])
 ### Root intro
 root_intro = str(server_data["root_intro"])
-### Video  
+### Video
 video_player = int(server_data["video"])
 adresse = str(server_data["adresse"])
 
 rate_voice = 110
+
 ### Path Snd
-crash_path = os.path.realpath(FOXDOT_ROOT + "/lib/Crashserver/crash_snd/")
+#crash_path = os.path.realpath(FOXDOT_ROOT + "/lib/Crashserver/crash_snd/")
+crash_path = "/home/zbdm/crashserver/ultimate_crash_sample/"
 sample_description_path = os.path.join(crash_path, "description.cs")
 if os.path.isfile(sample_description_path):
 	with open(sample_description_path, "rb") as file:
 		sample_description = pickle.load(file)
+
 gamme = ["locrianMajor", "locrian", "phrygian", "minor", "dorian", "mixolydian", "major", "lydian", "lydianAug"]
 crash_function = ["lost", "binary", "desynchro", "PTime", "PTimebin" "lininf", "PDrum", "darker", "lighter", \
 "human", "unison", "ascii_gen", "attack", "PChords", "fourths", "thirds", "seconds", "duree", "print_synth", "print_sample", "print_fx", "PChain2"]
@@ -74,7 +79,7 @@ crash_function = ["lost", "binary", "desynchro", "PTime", "PTimebin" "lininf", "
 # scale_intro = "minor"
 # ### Root intro
 # root_intro = "E"
-# ### Video  
+# ### Video
 # video = 0
 # adresse = "192.168.0.22"
 
@@ -98,7 +103,7 @@ except:
 	print("Error importing Speech Extension")
 
 ### Foxdot tools
-try:    
+try:
 	from .Crashserver.arpy import *
 	from .Crashserver.sdur import *
 	from .Crashserver.drumspattern import *
@@ -116,7 +121,7 @@ except:
 	print("Error in generating weapons code")
 
 
-try: 
+try:
 	FOXDOT_SND   = crash_path
 	FOXDOT_LOOP  = os.path.realpath(crash_path + "/_loop_/")
 	FoxDotCode.use_sample_directory(FOXDOT_SND)
@@ -152,8 +157,8 @@ def init_voice():
 		Voice(txt_init, rate=rate_voice, amp=1, lang=lang, voice=voice)
 		#def txt_intro():
 		#   Voice(crash_txt, rate=rate_voice, amp=1, lang=lang, voice=voice)
-			
-		#Clock.future(tmps, lambda: txt_intro())    
+
+		#Clock.future(tmps, lambda: txt_intro())
 	else:
 		print("Sorry, we crash only from windows or linux")
 
@@ -171,9 +176,9 @@ def calc_dur_voice(phrase=""):
 	except:
 		return 8
 
-### Generate ASCII from text into the clipboard 
+### Generate ASCII from text into the clipboard
 def ascii_gen(text=""):
-	if text is not None:
+	if text != None:
 		clip.copy(figlet_format(text))
 
 ##############   BEGIN ##############################################
@@ -198,13 +203,13 @@ def connect(video=video):
 
 
 def attack(part="", active_voice=1):
-	if type(part) is not str:  ### so we can type attack(42) or attack(43)
+	if type(part) != str:  ### so we can type attack(42) or attack(43)
 		part = str(part)
 
 	### next part
-	elif part == "":    
+	elif part == "":
 		part = lost_played[0]
-	
+
 	if part in lost_played:
 		lost_played.remove(part)
 
@@ -215,30 +220,30 @@ def attack(part="", active_voice=1):
 	### Define prompt
 	exten = ''.join(choice(string.ascii_lowercase) for x in range(3))
 	prompt = "### attack@{}.{}:~$ ".format(part, exten)
-	
+
 	### Init server
 	if part == "init":
-		if active_voice == 1:    
+		if active_voice == 1:
 			init_voice()
 		global time_init
 		time_init = time.time()
 		clip.copy(figlet_format(blase) + "\n" + prompt + define_virus()+ "\n" + code_txt)
-	
+
 	### Random code generator
 	if part == "42" or part == "random":   ### Random Synth code
 		clip.copy(prompt + define_virus()+ "\n" + random_virus())
 	elif part=="43":  ### Random play code
 		clip.copy(prompt + define_virus()+ "\n" + random_virus_char())
-	
+
 	### Select Part and generate Ascii text
 	else:
-		clip.copy((figlet_format(blase) if blase is not None else "") + "\n" + prompt + define_virus()+ "\n" + (code_txt if code_txt is not None else ""))
-	
+		clip.copy((figlet_format(blase) if blase != None else "") + "\n" + prompt + define_virus()+ "\n" + (code_txt if code_txt is not None else ""))
+
 	### Generate Voice
-	if voice_txt is not None:   ### Voice generator
-		if voice_txt is not "" and active_voice==1:
+	if voice_txt != None:   ### Voice generator
+		if voice_txt != "" and active_voice==1:
 			voice_lpf(400)
-			Voice(voice_txt, rate=rate_voice + randint(0,20), lang=lang, voice=randint(1,5))        
+			Voice(voice_txt, rate=rate_voice + randint(0,20), lang=lang, voice=randint(1,5))
 			Clock.future(calc_dur_voice(voice_txt), lambda: voice_lpf(0))
 
 ################# END #################################################
@@ -248,7 +253,7 @@ import random
 
 @player_method
 def unison(self, unison=2, detune=0.125, analog=40):
-	""" Like spread(), but can specify number of voices(unison)  
+	""" Like spread(), but can specify number of voices(unison)
 	Sets pan to (-1,-0.5,..,0.5,1) and pshift to (-0.125,-0.0625,...,0.0625,0.125)
 	If unison is odd, an unchanged voice is added in the center
 	Eg : p1.unison(4, 0.5) => pshift=(-0.5,-0.25,0.25,0.5), pan=(-1.0,-0.5,0.5,1.0)
@@ -266,7 +271,7 @@ def unison(self, unison=2, detune=0.125, analog=40):
 			pshift.insert(0,detune*-(i/(uni/2))+PWhite(0,-1*detune*(analog/100)))
 		if unison%2!=0 and unison > 1:
 			pan.insert(int(len(pan)/2), 0)
-			pshift.insert(int(len(pan)/2), 0)              
+			pshift.insert(int(len(pan)/2), 0)
 		self.pan = tuple(pan)
 		self.pshift = tuple(pshift)
 	else:
@@ -309,7 +314,7 @@ def fill(self, on=1):
 
 @player_method
 class PChain2(RandomGenerator):
-	""" An example of a Markov Chain generator pattern. The mapping argument 
+	""" An example of a Markov Chain generator pattern. The mapping argument
 		should be a dictionary of keys whose values are a list/pattern of possible
 		destinations.  """
 	def __init__(self, mapping, **kwargs):
@@ -332,7 +337,7 @@ class PChain2(RandomGenerator):
 		self.last_value = random.choices(key, prob)[0]
 		return self.last_value
 
-# END OF PLAYERS METHODS        
+# END OF PLAYERS METHODS
 
 def lost(total=0):
 	global lost_played
@@ -344,13 +349,13 @@ def lost(total=0):
 	elif total==2:
 		print("Reinit lost")
 		lost_played=lost_list[:]
-		print(lost_played)		
+		print(lost_played)
 
 def print_video():
 	clip.copy("v1 >> video(vid1=0, vid2=0, vid1rate=1, vid2rate=1, vid1kal=0, vid2kal=0, vid1glitch=0, vid2glitch=0, vidblend=0, vidmix=0, vid1index=0, vid2index=0)")
 
 def binary(number):
-	# return a list converted to binary from a number 
+	# return a list converted to binary from a number
 	binlist = [int(i) for i in str(bin(number)[2:])]
 	return binlist
 
@@ -361,11 +366,11 @@ def duree():
 
 
 def desynchro():
-	clip.copy(random_bpm())     
+	clip.copy(random_bpm())
 
 def PTime():
 	### Generate a pattern from the local machine time
-	return [int(t) for t in str(Clock.get_time_at_beat(int(Clock.now()))) if t is not '.']
+	return [int(t) for t in str(Clock.get_time_at_beat(int(Clock.now()))) if t != '.']
 
 def PTimebin():
 	### Generate a pattern of actual time converted to binary
@@ -378,7 +383,7 @@ def PDrum(style=None):
 	''' Generate a drum pattern style '''
 	if style == None:
 		print(DrumsPattern.keys())
-	else:   
+	else:
 		clip.copy(DrumsPattern[style])
 
 
@@ -389,7 +394,7 @@ def darker():
 	if Scale.default.name == gamme[0]:
 		print("Darkest scale reach !")
 	else:
-		actual = Scale.default.name        
+		actual = Scale.default.name
 		Scale.default = gamme[gamme.index(actual) - 1]
 
 def lighter():
@@ -399,7 +404,7 @@ def lighter():
 	if Scale.default.name == gamme[-1]:
 		print("Lightest scale reach !")
 	else:
-		actual = Scale.default.name        
+		actual = Scale.default.name
 		Scale.default = gamme[gamme.index(actual) + 1]
 
 class PChords(GeneratorPattern):
@@ -412,7 +417,7 @@ class PChords(GeneratorPattern):
 		self.list_of_choice = []
 	def func(self, index, list_of_choice=[]):
 		self.list_of_choice = []
-		if self.chord is None:
+		if self.chord == None:
 			self.chord = tuple(self.list_chords[choice(list(self.list_chords))])
 		for keys, values in self.list_chords.items():
 			for note in values:
@@ -430,7 +435,7 @@ def print_video():
 def print_synth(synth=""):
 	### Show the name and the args of a custom synth
 	path = os.path.realpath(FOXDOT_ROOT + "/osc/scsyndef/")
-	if synth == "":    
+	if synth == "":
 		dir_list = os.listdir(path)
 		synth_list = []
 		for p in dir_list:
@@ -441,16 +446,16 @@ def print_synth(synth=""):
 		path = os.path.realpath(FOXDOT_ROOT + "/osc/scsyndef/" + synth + ".scd")
 		with open(str(path), "r") as synth:
 			synth = synth.readlines()
-		synth_txt = [line.strip() for line in synth if line is not "\n"]
+		synth_txt = [line.strip() for line in synth if line != "\n"]
 		txt = str(''.join(synth_txt))
 		synthname = re.findall('SynthDef[.new]*[(\\\]*(.+?),',txt)
-		synthargs = re.findall('\{\|(.*)\|', txt)    
+		synthargs = re.findall('\{\|(.*)\|', txt)
 		print(str(synthname[0]), " : ", str(synthargs[0]))
 
 def print_fx(fx=""):
 	### Show the name and the args of a custom synth
 	path = os.path.realpath(FOXDOT_ROOT + "/osc/sceffects/")
-	if fx == "":    
+	if fx == "":
 		dir_list = os.listdir(path)
 		fx_list = []
 		for p in dir_list:
@@ -461,10 +466,10 @@ def print_fx(fx=""):
 		path = os.path.realpath(FOXDOT_ROOT + "/osc/sceffects/" + fx + ".scd")
 		with open(str(path), "r") as fx:
 			fx = fx.readlines()
-		fx_txt = [line.strip() for line in fx if line is not "\n"]
+		fx_txt = [line.strip() for line in fx if line != "\n"]
 		txt = str(''.join(fx_txt))
 		fxname = re.findall('SynthDef[.new]*[(\\\]*(.+?),',txt)
-		fxargs = re.findall('\{\|(.*)\|', txt)    
+		fxargs = re.findall('\{\|(.*)\|', txt)
 		print(str(fxname[0]), " : ", str(fxargs[0]))
 
 def print_sample(sample=""):
@@ -473,15 +478,22 @@ def print_sample(sample=""):
 		print("")
 		for k, v in sorted(sample_description.items(), key= lambda x: x[0].casefold()):
 			print(k.ljust(2, " ") + ': ' + v.ljust(40), end="")
-		print("")	
+		print("")
 	else:
-		if sample in alpha or sample in nonalpha:
-			print(f'{sample}: {sample_description[sample]}')
+		if len(sample) == 1:
+			if sample.lower() in alpha or sample.upper() in alpha or sample in nonalpha:
+				print(f'{sample}: {sample_description[sample]}')
 		else:
 			for key, value in sample_description.items():
 				if sample.lower() in value.lower():
 					print(f'{key}: {value}')
 
+def print_loops(loop=""):
+	if loop=="":
+		print(loops)
+	else:
+		listloops = sorted([fn.rsplit(".",1)[0] for fn in os.listdir(os.path.join(FOXDOT_LOOP, loop))])
+		print(listloops)
 
 ### Chord progression, Root mouvement by fourths, thirds, seconds
 fourths = PChain({
@@ -491,7 +503,7 @@ fourths = PChain({
 	IV: [VII, I],
 	V: [I, II],
 	VI: [II, III],
-	VII: [III, IV]   
+	VII: [III, IV]
 })
 
 thirds = PChain({
@@ -501,7 +513,7 @@ thirds = PChain({
 	IV: [II, VI],
 	V: [III, VII],
 	VI: [I, IV],
-	VII: [II, V]   
+	VII: [II, V]
 })
 
 seconds = PChain({
@@ -511,7 +523,7 @@ seconds = PChain({
 	IV: [III, V],
 	V: [IV, VI],
 	VI: [V, VII],
-	VII: [VI, I]   
+	VII: [VI, I]
 })
 
 def melody(scale_melody=Scale.default.name):
@@ -527,14 +539,14 @@ def melody(scale_melody=Scale.default.name):
 		8: [[0,1,2,3,4,5,6,7,8,9,10,11], [0,0,0,0,0,27,0,55,0,18,0,0]],
 		9: [[0,1,2,3,4,5,6,7,8,9,10,11], [4,0,2,0,1,5,0,60,0,19,1,8]],
 		10: [[0,1,2,3,4,5,6,7,8,9,10,11], [17,0,2,1,12,2,0,22,1,18,24,2]],
-		11: [[0,1,2,3,4,5,6,7,8,9,10,11], [45,0,12,0,0,1,0,7,0,26,0,9]]                     
+		11: [[0,1,2,3,4,5,6,7,8,9,10,11], [45,0,12,0,0,1,0,7,0,26,0,9]]
 		}
 
 	scale_melody_dict = {key: melody_dict[key] for key in melody_dict.keys() if key in Scale[scale_melody]}
 	for keys, values in scale_melody_dict.items():
 		note, prob = values
 		fnote, fprob = zip(*((key, pro) for key, pro in zip(note, prob) if key in Scale[scale_melody]))
-		scale_melody_dict[keys] = [list(fnote), list(fprob)]                  
+		scale_melody_dict[keys] = [list(fnote), list(fprob)]
 	return PChain2(scale_melody_dict)
 
 
@@ -544,7 +556,7 @@ chords = {
 	III: [[I, II, III, IV, V, VI], [0,5,0,85,2,8]],
 	IV: [[I, II, III, IV, V, VI], [20,1,1,1,76,1]],
 	V: [[I, II, III, IV, V, VI], [70,1,2,13,1,14]],
-	VI: [[I, II, III, IV, V, VI], [5,5,1,49,39,1]], 
+	VI: [[I, II, III, IV, V, VI], [5,5,1,49,39,1]],
 	}
 
 krhytm = {
@@ -554,13 +566,13 @@ krhytm = {
 	0.375: [0.375,0.375,1],
 	1: [0.75,0.25],
 	0.75: [0.25,1,0.125,0.125]
-	}   
+	}
 
 from .Crashserver.chords_dict import *
 
 @player_method
 class PMarkov(RandomGenerator):
-	""" An example of a Markov Chain generator pattern. The mapping argument 
+	""" An example of a Markov Chain generator pattern. The mapping argument
 		should be a dictionary of keys whose values are a list/pattern of possible
 		destinations.  """
 	def __init__(self, init_value="", **kwargs):
@@ -574,7 +586,7 @@ class PMarkov(RandomGenerator):
 		self.init_random(**kwargs)
 		self.turn = 0
 	def stream_value(self, active_dict):
-		""" Return mapping dict with a list of keys and probability""" 
+		""" Return mapping dict with a list of keys and probability"""
 		for key, value in active_dict.items():
 			self.mapping[key] = [asStream(value[0]), asStream(value[1])]
 			#self.last_value = key
@@ -590,7 +602,7 @@ class PMarkov(RandomGenerator):
 		return random.choices(key, prob)[0]
 	def func(self, index):
 		if self.turn == 0:
-			if self.init_value is "":
+			if self.init_value == "":
 				self.init_value = random.choice(list(cho1.keys()))
 			self.active_value = self.init_value
 			#print("Turn : {}, chord : {}".format(self.turn, self.active_value))
@@ -639,12 +651,12 @@ def switch(self, other, key, bypass=1):
 
 
 def drop(duree=32, nbr=0, end=4):
-	""" Create a drop effect (var bpm), 
-		duree = durée totale de la partie, 
+	""" Create a drop effect (var bpm),
+		duree = durée totale de la partie,
 		nbr = nombre de division du drop,
 		end = duree du drop en partant de la fin
 		pour retablir le tempo simplement drop(92)
-		""" 
+		"""
 	if nbr == 0:
 		Clock.bpm = duree
 	else:
@@ -652,38 +664,38 @@ def drop(duree=32, nbr=0, end=4):
 		actual_bpm = Clock.bpm
 		divi = 1
 		bpm_list = []
-		duree_list = []    
+		duree_list = []
 		for i in range(nbr):
-			actual_bpm /= divi 
+			actual_bpm /= divi
 			if actual_bpm < 10:
 				actual_bpm = 10
 			bpm_list.append(int(actual_bpm))
 			if i == 0:
 				duree_list.append(duree-end)
 			else:
-				duree_list.append(end/(2**i))    
+				duree_list.append(end/(2**i))
 			divi = 2
 		duree_list[-1] = end-sum(duree_list[1:-1])
 		Clock.bpm = var([bpm_list], [duree_list], start=Clock.mod(8))
-		print('var({}, {})'.format([bpm_list], [duree_list]))        
+		print('var({}, {})'.format([bpm_list], [duree_list]))
 
 
 def PMorse(text, point=1/4, tiret=3/4):
-	MORSE_DICT = { 'A':'.-', 'B':'-...', 
-					'C':'-.-.', 'D':'-..', 'E':'.', 
-					'F':'..-.', 'G':'--.', 'H':'....', 
-					'I':'..', 'J':'.---', 'K':'-.-', 
-					'L':'.-..', 'M':'--', 'N':'-.', 
-					'O':'---', 'P':'.--.', 'Q':'--.-', 
-					'R':'.-.', 'S':'...', 'T':'-', 
-					'U':'..-', 'V':'...-', 'W':'.--', 
-					'X':'-..-', 'Y':'-.--', 'Z':'--..', 
-					'1':'.----', '2':'..---', '3':'...--', 
-					'4':'....-', '5':'.....', '6':'-....', 
-					'7':'--...', '8':'---..', '9':'----.', 
-					'0':'-----', ', ':'--..--', '.':'.-.-.-', 
-					'?':'..--..', '/':'-..-.', '-':'-....-', 
-					'(':'-.--.', ')':'-.--.-'} 
+	MORSE_DICT = { 'A':'.-', 'B':'-...',
+					'C':'-.-.', 'D':'-..', 'E':'.',
+					'F':'..-.', 'G':'--.', 'H':'....',
+					'I':'..', 'J':'.---', 'K':'-.-',
+					'L':'.-..', 'M':'--', 'N':'-.',
+					'O':'---', 'P':'.--.', 'Q':'--.-',
+					'R':'.-.', 'S':'...', 'T':'-',
+					'U':'..-', 'V':'...-', 'W':'.--',
+					'X':'-..-', 'Y':'-.--', 'Z':'--..',
+					'1':'.----', '2':'..---', '3':'...--',
+					'4':'....-', '5':'.....', '6':'-....',
+					'7':'--...', '8':'---..', '9':'----.',
+					'0':'-----', ', ':'--..--', '.':'.-.-.-',
+					'?':'..--..', '/':'-..-.', '-':'-....-',
+					'(':'-.--.', ')':'-.--.-'}
 	morse = []
 	for l in text.split(" "):
 		for w in l:
@@ -693,7 +705,7 @@ def PMorse(text, point=1/4, tiret=3/4):
 				elif i == "-":
 					morse.append(tiret)
 			morse.append(rest(5*point))
-	morse[-1] += rest(2*point) 			
+	morse[-1] += rest(2*point)
 	return morse
 
 
