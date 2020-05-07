a812869ee9a095e915ca92a473e1b5cfa31be43b
zbdm <lagouttedo@gmail.com>
2020-05-07 16:36:06 +0200

    mod buffer to add all num

--- FoxDot/lib/Buffers.py
+++ FoxDot/lib/Buffers.py
@@ -46,10 +46,16 @@ nonalpha = {"&" : "ampersand",
             "?" : "question",
             "~" : "tilde",
             "\\" :"backslash",
+            "0" : "0",
             "1" : "1",
             "2" : "2",
             "3" : "3",
-            "4" : "4" }
+            "4" : "4",
+            "5" : "5",
+            "6" : "6",
+            "7" : "7",
+            "8" : "8",
+            "9" : "9"}
 
 DESCRIPTIONS = { 'a' : "Gameboy hihat",      'A' : "Gameboy kick drum",
                  'b' : "Noisy beep",         'B' : "Short saw",
