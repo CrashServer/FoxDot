83cd1e832ceeb12c8fb5ccf1f603dad076b3af75
Crash Server <47175530+CrashServer@users.noreply.github.com>
2020-08-14 14:09:20 +0200

    added ringz

--- FoxDot/lib/Crashserver/crashFX.py
+++ FoxDot/lib/Crashserver/crashFX.py
@@ -310,6 +310,11 @@ fx.add("for(1, 4, {|i| delayedSignal = AllpassL.ar(delayedSignal, 0.01 * 4.recip
 fx.add("osc = osc + delayedSignal")
 fx.save()
 
+fx = FxList.new('ringz','Ringmod', {'ringzfreq': 50, 'ringz': 0.1}, order=2)
+fx.doc("Ringmodulation")
+fx.add('osc = Ringz.ar(osc, freq: ringzfreq, decaytime: ringz, mul: 0.05)')
+fx.save()
+
 fx = FxList.new('room2', 'reverb_stereo', {'room2': 0, 'mix2': 0.2, 'damp2':0.8, 'revatk':0, 'revsus':1}, order=2)
 fx.add_var("dry")
 fx.add("dry = osc")
